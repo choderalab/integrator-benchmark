7e98c91d24d60158856cccfda630e9f67b245dd7
Josh Fass <jf694@cornell.edu>
2017-01-24 13:51:02 -0500

    Delete unused plot.py

--- code/plot.py
+++
@@ -1,8 +0,0 @@
-from integrator_benchmark import plot
-
-from cPickle import load
-name = "constrained_randomized"
-f = open(name + "_results.pkl", "r")
-results = load(f)
-f.close()
-plot(results, name)
