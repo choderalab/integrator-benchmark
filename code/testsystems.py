8457ce6872865ba7dc21d756d1e9cef1559be681
Josh Fass <jf694@cornell.edu>
2017-01-31 13:38:38 -0500

    Add explicit-solvent AlanineDipeptide testsystem

--- code/testsystems.py
+++ code/testsystems.py
@@ -1,6 +1,6 @@
 import numpy as np
 from openmmtools.testsystems import HarmonicOscillator, PowerOscillator, \
-    AlanineDipeptideVacuum, WaterBox
+    AlanineDipeptideVacuum, WaterBox, AlanineDipeptideExplicit
 from simtk.openmm import app
 
 from utils import configure_platform
@@ -24,22 +24,42 @@ def load_waterbox(constrained=True):
     positions = np.load("waterbox.npy") # load equilibrated configuration saved beforehand
     return topology, system, positions
 
+def keep_only_some_forces(system):
+    """Remove unwanted forces, e.g. center-of-mass motion removal"""
+    forces_to_keep = ["HarmonicBondForce", "HarmonicAngleForce",
+                      "PeriodicTorsionForce", "NonbondedForce"]
+    force_indices_to_remove = list()
+    for force_index in range(system.getNumForces()):
+        force = system.getForce(force_index)
+        if force.__class__.__name__ not in forces_to_keep:
+            force_indices_to_remove.append(force_index)
+    for force_index in force_indices_to_remove[::-1]:
+        print('   Removing %s' % system.getForce(force_index).__class__.__name__)
+        system.removeForce(force_index)
 
 def load_alanine(constrained=True):
+    """Load AlanineDipeptide vacuum, optionally with hydrogen bonds constrained"""
     if constrained: constraints = app.HBonds
     else: constraints = None
     testsystem = AlanineDipeptideVacuum(constraints=constraints)
     topology, system, positions = testsystem.topology, testsystem.system, testsystem.positions
 
-    index_of_CMMotionRemover = -1
-    for i in range(system.getNumForces()):
-        if type(system.getForce(i)) == mm.CMMotionRemover:
-            index_of_CMMotionRemover = i
-    if index_of_CMMotionRemover != -1:
-        system.removeForce(i)
+    keep_only_some_forces(system)
 
     return topology, system, positions
 
+def load_solvated_alanine(constrained=True):
+    """Load AlanineDipeptide in explicit solvent,
+    optionally with rigid water + hydrogen bonds constrained."""
+    args = {"ewaldErrorTolerance":1e-5,
+            "nonbondedCutoff":10*unit.angstroms
+            }
+    if constrained:
+        testsystem = AlanineDipeptideExplicit(constraints=app.HBonds, rigid_water=True, **args)
+    else:
+        testsystem = AlanineDipeptideExplicit(constraints=None, rigid_water=False, **args)
+    topology, system, positions = testsystem.topology, testsystem.system, testsystem.positions
+    return topology, system, positions
 
 simple_params = {
     "platform": configure_platform("Reference"),
