dda68995188d37ee1e1ac1f1394abf9456a66998
Josh Fass <jf694@cornell.edu>
2017-03-09 16:56:14 -0500

    Remove dead code from serialize_testsystems

--- benchmark/serialization/serialize_testsystems.py
+++ benchmark/serialization/serialize_testsystems.py
@@ -1,6 +1,6 @@
 # also serialize all the testsystems
 from simtk.openmm import XmlSerializer
-from code.testsystems import system_params
+from benchmark.testsystems import system_params
 
 for name in system_params.keys():
     print(name)
@@ -10,13 +10,3 @@ for name in system_params.keys():
         if not constrained: c = "unconstrained"
         with open("serialized_testsystems/{}_{}.xml".format(name, c), "w") as f:
             f.writelines(XmlSerializer.serialize(sys))
-
-    ## also pretty-print
-    #readable_lines = []
-    #for i in range(integrator.getNumComputations()):
-    #    step_type, target, expr = integrator.getComputationStep(i)
-    #    readable_lines.append(step_type_dict[step_type].format(target=target, expr=expr) + "\n")
-    #print(readable_lines)
-
-    #with open("serialized_integrators/readable_{}.txt".format(scheme), "w") as f:
-    #    f.writelines(readable_lines)
