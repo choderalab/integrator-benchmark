493c7494406c15ea1c86f9788a077322a113ab3e
Josh Fass <jf694@cornell.edu>
2017-03-17 17:02:39 -0400

    Quick comparison with nonparametric estimator

--- benchmark/experiments/toy/estimate_equilibrium_free_energy.py
+++ benchmark/experiments/toy/estimate_equilibrium_free_energy.py
@@ -1,7 +1,7 @@
 # Get precise estimates for the equilibrium free energy, by
 # numerical quadrature.
 # We will use this for:
-# * Validating the non-parametric estimator of entropy and free energy
+# * Validating the nonparametric estimator of entropy and free energy
 # * Computing nonequilibrium free energy differences
 
 from quartic_simple import potential, kinetic_energy, log_p, log_v_density, beta
@@ -37,5 +37,36 @@ average_energy = average_potential + average_kinetic_energy
 print("<E> = {}".format(average_energy))
 print("S = {}".format(entropy))
 
-print("Equilibrium free energy: {}".format(average_energy - entropy / beta))
-print("x-marginal equilibrium free energy: {}".format(average_potential - entropy_x / beta))
+equilibrium_free_energy = average_energy - entropy / beta
+x_marginal_free_energy = average_potential - entropy_x / beta
+
+print("Equilibrium free energy: {}".format(equilibrium_free_energy))
+print("x-marginal equilibrium free energy: {}".format(x_marginal_free_energy))
+
+# For comparison, let's estimate the entropy using the k-nearest-neighbor estimator
+
+from benchmark.evaluation.entropy import estimate_entropy, estimate_marginal_entropies
+from quartic_simple import velocity_scale
+from benchmark import DATA_PATH
+import os
+n_samples = 100000
+eq_xs = np.load(os.path.join(DATA_PATH, "quartic_x_equilibrium.npy"))
+np.random.shuffle(eq_xs)
+eq_xs = eq_xs[:n_samples]
+eq_vs = np.random.randn(n_samples) * velocity_scale
+
+xv = np.vstack((eq_xs, eq_vs)).T
+print(xv.shape)
+
+for k in range(1, 10):
+    print("k={}".format(k))
+    estimated_marginal_entropies = estimate_marginal_entropies(xv, k)
+    estimated_entropy_x, estimated_entropy_v = estimated_marginal_entropies
+    print("\tEstimated marginal entropies of p(x) and p(v): {}".format(estimated_marginal_entropies))
+    estimated_joint_entropy = estimate_entropy(xv, k)
+    print("\tEstimated joint entropy of p(x,v): {}".format(estimated_joint_entropy))
+
+    estimated_equilibrium_free_energy = average_energy - estimated_joint_entropy / beta
+    estimated_x_marginal_free_energy = average_potential - estimated_entropy_x / beta
+    print("\tEstimated equilibrium free energy: {}".format(estimated_equilibrium_free_energy))
+    print("\tEstimated x-marginal equilibrium free energy: {}".format(estimated_x_marginal_free_energy))
