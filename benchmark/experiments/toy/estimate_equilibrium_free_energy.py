c0fceda9b55d26610a2b489062e61e2edfa051d4
Josh Fass <jf694@cornell.edu>
2017-03-17 18:31:29 -0400

    Move experiment into main

--- benchmark/experiments/toy/estimate_equilibrium_free_energy.py
+++ benchmark/experiments/toy/estimate_equilibrium_free_energy.py
@@ -44,29 +44,29 @@ print("Equilibrium free energy: {}".format(equilibrium_free_energy))
 print("x-marginal equilibrium free energy: {}".format(x_marginal_free_energy))
 
 # For comparison, let's estimate the entropy using the k-nearest-neighbor estimator
+if __name__ == "__main__":
+    from benchmark.evaluation.entropy import estimate_entropy, estimate_marginal_entropies
+    from quartic_simple import velocity_scale
+    from benchmark import DATA_PATH
+    import os
+    n_samples = 100000
+    eq_xs = np.load(os.path.join(DATA_PATH, "quartic_x_equilibrium.npy"))
+    np.random.shuffle(eq_xs)
+    eq_xs = eq_xs[:n_samples]
+    eq_vs = np.random.randn(n_samples) * velocity_scale
 
-from benchmark.evaluation.entropy import estimate_entropy, estimate_marginal_entropies
-from quartic_simple import velocity_scale
-from benchmark import DATA_PATH
-import os
-n_samples = 100000
-eq_xs = np.load(os.path.join(DATA_PATH, "quartic_x_equilibrium.npy"))
-np.random.shuffle(eq_xs)
-eq_xs = eq_xs[:n_samples]
-eq_vs = np.random.randn(n_samples) * velocity_scale
+    xv = np.vstack((eq_xs, eq_vs)).T
+    print(xv.shape)
 
-xv = np.vstack((eq_xs, eq_vs)).T
-print(xv.shape)
+    for k in range(1, 10):
+        print("k={}".format(k))
+        estimated_marginal_entropies = estimate_marginal_entropies(xv, k)
+        estimated_entropy_x, estimated_entropy_v = estimated_marginal_entropies
+        print("\tEstimated marginal entropies of p(x) and p(v): {}".format(estimated_marginal_entropies))
+        estimated_joint_entropy = estimate_entropy(xv, k)
+        print("\tEstimated joint entropy of p(x,v): {}".format(estimated_joint_entropy))
 
-for k in range(1, 10):
-    print("k={}".format(k))
-    estimated_marginal_entropies = estimate_marginal_entropies(xv, k)
-    estimated_entropy_x, estimated_entropy_v = estimated_marginal_entropies
-    print("\tEstimated marginal entropies of p(x) and p(v): {}".format(estimated_marginal_entropies))
-    estimated_joint_entropy = estimate_entropy(xv, k)
-    print("\tEstimated joint entropy of p(x,v): {}".format(estimated_joint_entropy))
-
-    estimated_equilibrium_free_energy = average_energy - estimated_joint_entropy / beta
-    estimated_x_marginal_free_energy = average_potential - estimated_entropy_x / beta
-    print("\tEstimated equilibrium free energy: {}".format(estimated_equilibrium_free_energy))
-    print("\tEstimated x-marginal equilibrium free energy: {}".format(estimated_x_marginal_free_energy))
+        estimated_equilibrium_free_energy = average_energy - estimated_joint_entropy / beta
+        estimated_x_marginal_free_energy = average_potential - estimated_entropy_x / beta
+        print("\tEstimated equilibrium free energy: {}".format(estimated_equilibrium_free_energy))
+        print("\tEstimated x-marginal equilibrium free energy: {}".format(estimated_x_marginal_free_energy))
