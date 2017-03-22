9b32cb81c237990ea9a1df1d9e20e01ed103210f
Josh Fass <jf694@cornell.edu>
2017-03-22 12:03:13 -0400

    Add to numba factories to integrators.__all__

--- benchmark/integrators/__init__.py
+++ benchmark/integrators/__init__.py
@@ -1,4 +1,6 @@
 from ghmc import CustomizableGHMC
 from langevin import LangevinSplittingIntegrator
+from numba_integrators import baoab_factory, vvvr_factory, metropolis_hastings_factory
 
-__all__ = ["CustomizableGHMC", "LangevinSplittingIntegrator"]
+__all__ = ["CustomizableGHMC", "LangevinSplittingIntegrator",
+           "baoab_factory", "vvvr_factory", "metropolis_hastings_factory"]
