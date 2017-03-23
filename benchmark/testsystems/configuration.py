4cb060d3ab159175d2c36ed1f87dee5af713eef9
Josh Fass <jf694@cornell.edu>
2017-03-23 15:42:15 -0400

    Revert to mixed precision in OpenCL

--- benchmark/testsystems/configuration.py
+++ benchmark/testsystems/configuration.py
@@ -8,7 +8,7 @@ def configure_platform(platform_name='Reference'):
         platform = mm.Platform.getPlatformByName('Reference')
     elif platform_name.upper() == 'OpenCL'.upper():
         platform = mm.Platform.getPlatformByName('OpenCL')
-        platform.setPropertyDefaultValue('OpenCLPrecision', 'double')
+        platform.setPropertyDefaultValue('OpenCLPrecision', 'mixed')
     elif platform_name.upper() == 'CUDA'.upper():
         platform = mm.Platform.getPlatformByName('CUDA')
         platform.setPropertyDefaultValue('CUDAPrecision', 'double')
