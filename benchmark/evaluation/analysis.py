7ddd94f54e349265e79f38d29c9e0f90e2914445
Josh Fass <jf694@cornell.edu>
2017-03-23 18:33:54 -0400

    Less ambiguous variable name in analysis

--- benchmark/evaluation/analysis.py
+++ benchmark/evaluation/analysis.py
@@ -13,8 +13,8 @@ def estimate_nonequilibrium_free_energy(W_shads_F, W_shads_R, N_eff=None):
     if N_eff == None: N_eff = len(W_shads_F)
     DeltaF_neq = 0.5 * (np.mean(W_shads_F) - np.mean(W_shads_R))
     # squared uncertainty = [var(W_0->M) + var(W_M->2M) - 2 cov(W_0->M, W_M->2M)] / (4 N_eff)
-    sq_uncertainty = (np.var(W_shads_F) + np.var(W_shads_R) - 2 * np.cov(W_shads_F, W_shads_R)[0,1]) / (4 * N_eff)
-    return DeltaF_neq, sq_uncertainty
+    squared_uncertainty = (np.var(W_shads_F) + np.var(W_shads_R) - 2 * np.cov(W_shads_F, W_shads_R)[0,1]) / (4 * N_eff)
+    return DeltaF_neq, squared_uncertainty
 
 def compute_free_energy(xv, potential, kinetic_energy, beta):
     """Given a potential energy function, kinetic energy function, and inverse-temperature beta,
