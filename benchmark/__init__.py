9fdf0bec41a916879c823dff62c0c2eefc6c05f1
Josh Fass <jf694@cornell.edu>
2017-04-25 12:33:26 -0400

    Fix import?

--- benchmark/__init__.py
+++ benchmark/__init__.py
@@ -2,3 +2,5 @@ import os
 
 FIGURE_PATH = os.path.join(os.path.dirname(__file__), '../figures/')
 DATA_PATH = os.path.join(os.path.dirname(__file__), '../data/')
+
+import evaluation, experiments, integrators, plotting, serialization, tests, testsystems, utilities, verification
