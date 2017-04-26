885c99c3c4658e25fab947f5e022a2d8c901b884
Josh Fass <jf694@cornell.edu>
2017-04-26 15:15:07 -0400

    Remove unnecessary imports

--- benchmark/__init__.py
+++ benchmark/__init__.py
@@ -2,5 +2,3 @@ import os
 
 FIGURE_PATH = os.path.join(os.path.dirname(__file__), '../figures/')
 DATA_PATH = os.path.join(os.path.dirname(__file__), '../data/')
-
-import evaluation, experiments, integrators, plotting, serialization, tests, testsystems, utilities, verification
