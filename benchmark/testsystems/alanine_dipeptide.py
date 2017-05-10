50fcdf85f8e2f9c912a99e88e371a14a4545e4e7
Josh Fass <jf694@cornell.edu>
2017-05-09 21:13:05 -0400

    Add barostat to solvated alanine dipeptide
    Also use simulation_parameters instead of thermodynamic_parameters

--- benchmark/testsystems/alanine_dipeptide.py
+++ benchmark/testsystems/alanine_dipeptide.py
@@ -7,8 +7,8 @@ from simtk.openmm import app
 from simtk import unit
 from benchmark.testsystems.configuration import configure_platform
 from benchmark.utilities import keep_only_some_forces
-from benchmark import thermodynamic_parameters
-
+from benchmark import simulation_parameters
+from benchmark.utilities import add_barostat
 
 def load_alanine(constrained=True):
     """Load AlanineDipeptide vacuum, optionally with hydrogen bonds constrained"""
@@ -32,13 +32,14 @@ def load_solvated_alanine(constrained=True):
     else:
         testsystem = AlanineDipeptideExplicit(constraints=None, rigid_water=False, **args)
     topology, system, positions = testsystem.topology, testsystem.system, testsystem.positions
+    add_barostat(system)
     return topology, system, positions
 
 n_samples = 1000 # number of samples to collect
 if 'TRAVIS' in os.environ:
     n_samples = 20 # reduce sampling for travis
 
-temperature = thermodynamic_parameters["temperature"]
+temperature = simulation_parameters["temperature"]
 from benchmark.testsystems.bookkeepers import EquilibriumSimulator
 top, sys, pos = load_alanine(constrained=True)
 alanine_constrained = EquilibriumSimulator(platform=configure_platform("Reference"),
