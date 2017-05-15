e22ccb818e58b5ae7e6991106e5691c0c2d2d276
Josh Fass <jf694@cornell.edu>
2017-05-15 13:42:53 -0400

    Update g-BAOAB experiment

--- benchmark/experiments/gbaoab.py
+++ benchmark/experiments/gbaoab.py
@@ -4,13 +4,7 @@
 from benchmark.testsystems import NonequilibriumSimulator
 from benchmark.integrators.langevin import LangevinSplittingIntegrator
 from benchmark import DATA_PATH
-import os
-import pickle
-import numpy as np
-from simtk import unit
-from benchmark.evaluation.analysis import estimate_nonequilibrium_free_energy
-from benchmark.plotting import plot_scheme_comparison
-from benchmark.testsystems import waterbox_constrained
+from benchmark.testsystems import waterbox_constrained, constraint_coupled_harmonic_oscillators, t4_constrained
 
 import os
 import pickle
@@ -18,19 +12,22 @@ import numpy as np
 from simtk import unit
 from benchmark.evaluation.analysis import estimate_nonequilibrium_free_energy
 from benchmark.plotting import plot_scheme_comparison
-from benchmark.testsystems import alanine_unconstrained
 
 if __name__ == "__main__":
-    n_protocol_samples, protocol_length = 50, 100
-    system_name = "waterbox_constrained"
-    equilibrium_simulator = waterbox_constrained
+    n_protocol_samples, protocol_length = 500, 100
+    #system_name = "constraint_coupled_harmonic_oscillators"
+    #equilibrium_simulator = constraint_coupled_harmonic_oscillators
+    #system_name = "waterbox_constrained"
+    #equilibrium_simulator = waterbox_constrained
+    system_name = "t4_constrained"
+    equilibrium_simulator = t4_constrained
     target_filename = os.path.join(DATA_PATH, "gbaoab_{}.pkl".format(system_name))
 
     schemes = {"g-BAOAB (1)": "V R O R V",
                "g-BAOAB (2)": "V R R O R R V",
                "g-BAOAB (3)": "V R R R O R R R V",
                "g-BAOAB (4)": "V R R R R O R R R R V"}
-    timesteps = np.linspace(5.0, 10.0, 5)
+    timesteps = np.linspace(1.0, 5.0, 5)
     noneq_simulators = {}
     for name, scheme in schemes.items():
         for timestep in timesteps:
