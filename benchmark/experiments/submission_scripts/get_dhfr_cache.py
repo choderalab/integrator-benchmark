5493f9357f49cff195280e8a1331fd16059a013d
Josh Fass <jf694@cornell.edu>
2017-05-16 15:42:46 -0400

    Add cluster submission script for populating DHFR sample cache

--- benchmark/experiments/submission_scripts/get_dhfr_cache.py
+++ benchmark/experiments/submission_scripts/get_dhfr_cache.py
@@ -1,3 +1,4 @@
-# dummy script: try to get a sample from the sample cache
+# Tries to get a sample from the sample cache.
+# This triggers populating the cache, if it doesn't already exist.
 from benchmark.testsystems import dhfr_constrained
 dhfr_constrained.sample_x_from_equilibrium()
