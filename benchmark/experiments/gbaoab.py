a6b650db02bff3930c39bae15141c0ca89326fe8
Josh Fass <jf694@cornell.edu>
2017-05-18 11:17:52 -0400

    Update g-BAOAB experiment + submission script

--- benchmark/experiments/gbaoab.py
+++ benchmark/experiments/gbaoab.py
@@ -4,7 +4,7 @@
 from benchmark.testsystems import NonequilibriumSimulator
 from benchmark.integrators.langevin import LangevinSplittingIntegrator
 from benchmark import DATA_PATH
-from benchmark.testsystems import waterbox_constrained, constraint_coupled_harmonic_oscillators, t4_constrained
+from benchmark.testsystems import dhfr_constrained
 
 import os
 import pickle
@@ -13,29 +13,27 @@ from simtk import unit
 from benchmark.evaluation.analysis import estimate_nonequilibrium_free_energy
 from benchmark.plotting import plot_scheme_comparison
 
-if __name__ == "__main__":
-    n_protocol_samples, protocol_length = 500, 100
-    #system_name = "constraint_coupled_harmonic_oscillators"
-    #equilibrium_simulator = constraint_coupled_harmonic_oscillators
-    #system_name = "waterbox_constrained"
-    #equilibrium_simulator = waterbox_constrained
-    system_name = "t4_constrained"
-    equilibrium_simulator = t4_constrained
-    target_filename = os.path.join(DATA_PATH, "gbaoab_{}.pkl".format(system_name))
+def run_experiment(n_geodesic_step_list=range(1,5), n_protocol_samples=500, protocol_length=100, friction="high"):
+    if friction == "high":
+        collision_rate = 91.0 / unit.picosecond
+    elif friction == "low":
+        collision_rate = 1.0 / unit.picosecond
+    system_name = "dhfr_constrained"
+    equilibrium_simulator = dhfr_constrained
+    target_filename = os.path.join(DATA_PATH, "gbaoab_{}_friction{}.pkl".format(friction, system_name))
 
-    schemes = {"g-BAOAB (1)": "V R O R V",
-               "g-BAOAB (2)": "V R R O R R V",
-               "g-BAOAB (3)": "V R R R O R R R V",
-               "g-BAOAB (4)": "V R R R R O R R R R V"}
-    timesteps = np.linspace(1.0, 5.0, 5)
+    timesteps = np.linspace(0.5, 4.5, 5)
     noneq_simulators = {}
-    for name, scheme in schemes.items():
-        for timestep in timesteps:
+    for timestep in timesteps:
+        for n_geodesic_steps in n_geodesic_step_list:
+            name = "g-BAOAB ({})".format(n_geodesic_steps)
+            Rs = ["R"] * n_geodesic_steps
+            scheme = " ".join(["V"] + Rs + ["O"] + Rs + ["V"])
             noneq_simulators[(name, timestep)] = NonequilibriumSimulator(equilibrium_simulator,
                                                          LangevinSplittingIntegrator(
                                                              splitting=scheme,
                                                              timestep=timestep * unit.femtosecond,
-                                                             collision_rate=91.0/unit.picoseconds))
+                                                             collision_rate=collision_rate))
     results = {}
     for marginal in ["configuration", "full"]:
         results[marginal] = {}
@@ -51,3 +49,8 @@ if __name__ == "__main__":
         pickle.dump(results, f)
 
     plot_scheme_comparison(target_filename, system_name)
+
+
+if __name__ == "__main__":
+    run_experiment(friction="high")
+    run_experiment(friction="low")
