1838fa7d4ec3c04062d46a0d59c3e0bfd5de3216
Josh Fass <jf694@cornell.edu>
2018-01-16 16:03:46 -0500

    Add tiny_waterbox

--- benchmark/testsystems/__init__.py
+++ benchmark/testsystems/__init__.py
@@ -1,6 +1,6 @@
 from .alanine_dipeptide import alanine_constrained, alanine_unconstrained
 from .alanine_dipeptide import solvated_alanine_constrained, solvated_alanine_unconstrained
-from .waterbox import flexible_waterbox, waterbox_constrained
+from .waterbox import flexible_waterbox, waterbox_constrained, tiny_waterbox
 from .coupled_power_oscillators import coupled_power_oscillators
 from .low_dimensional_systems import double_well, quartic, NumbaNonequilibriumSimulator
 from .bookkeepers import EquilibriumSimulator, NonequilibriumSimulator
@@ -18,5 +18,6 @@ system_loaders = {"alanine": load_alanine,
 
 __all__ = ["alanine_constrained", "alanine_unconstrained", "solvated_alanine_unconstrained",
            "flexible_waterbox", "waterbox_constrained", "coupled_power_oscillators",
+           "tiny_waterbox",
            "dhfr_constrained", "dhfr_unconstrained", "src_constrained", "t4_constrained", "t4_unconstrained", "constraint_coupled_harmonic_oscillators",
            "EquilibriumSimulator", "NonequilibriumSimulator", "double_well", "quartic", "NumbaNonequilibriumSimulator", "system_loaders"]
