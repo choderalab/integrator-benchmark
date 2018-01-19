a268ac45ba1bf10f4ffa21a96f8107642fabe440
Josh Fass <jf694@cornell.edu>
2018-01-19 09:26:08 -0500

    Add WaterCluster testsystem
    Allows smaller n_dof than WaterBox
    
    Testsystem definition to be removed when an openmmtools version
    containing WaterCluster is released

--- benchmark/testsystems/__init__.py
+++ benchmark/testsystems/__init__.py
@@ -5,6 +5,7 @@ from .coupled_power_oscillators import coupled_power_oscillators
 from .low_dimensional_systems import double_well, quartic, NumbaNonequilibriumSimulator
 from .bookkeepers import EquilibriumSimulator, NonequilibriumSimulator
 from .testsystems import dhfr_constrained, dhfr_unconstrained, t4_constrained, t4_unconstrained, constraint_coupled_harmonic_oscillators, src_constrained
+from .watercluster import water_cluster_rigid, water_cluster_flexible
 
 from .alanine_dipeptide import load_alanine, load_solvated_alanine
 from .testsystems import load_t4_implicit, load_dhfr_explicit, load_src_explicit
@@ -18,6 +19,6 @@ system_loaders = {"alanine": load_alanine,
 
 __all__ = ["alanine_constrained", "alanine_unconstrained", "solvated_alanine_unconstrained",
            "flexible_waterbox", "waterbox_constrained", "coupled_power_oscillators",
-           "tiny_waterbox",
+           "tiny_waterbox", "water_cluster_rigid", "water_cluster_flexible",
            "dhfr_constrained", "dhfr_unconstrained", "src_constrained", "t4_constrained", "t4_unconstrained", "constraint_coupled_harmonic_oscillators",
            "EquilibriumSimulator", "NonequilibriumSimulator", "double_well", "quartic", "NumbaNonequilibriumSimulator", "system_loaders"]
