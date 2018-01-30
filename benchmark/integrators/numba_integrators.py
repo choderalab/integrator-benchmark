6ff457eaa08576fd1dae1dac0556f1c80e6ef02d
Josh Fass <jf694@cornell.edu>
2018-01-30 15:58:46 -0500

    Add ORVRO Numba integrator
    In case we want a 1D figure with all four symmetric splittings...

--- benchmark/integrators/numba_integrators.py
+++ benchmark/integrators/numba_integrators.py
@@ -64,6 +64,57 @@ def vvvr_factory(potential, force, velocity_scale, m):
 
     return jit(simulate_vvvr)
 
+def orvro_factory(potential, force, velocity_scale, m):
+    potential = jit_if_possible(potential)
+    force = jit_if_possible(force)
+
+    def simulate_orvro(x0, v0, n_steps, gamma, dt):
+        """Simulate n_steps of orvro, accumulating heat
+        """
+        Q = 0
+        W_shads = np.zeros(n_steps)
+        x, v = x0, v0
+        xs, vs = np.zeros(n_steps), np.zeros(n_steps)
+        xs[0] = x0
+        vs[0] = v0
+        E_old = potential(x) + 0.5 * m * v**2
+
+        a = np.exp(-gamma * (dt / 2.0))
+        b = np.sqrt(1 - np.exp(-2 * gamma * (dt / 2.0)))
+
+        for i in range(1, n_steps):
+            # half O step
+            ke_old = 0.5 * m * v**2
+            v = (a * v) + b * velocity_scale * np.random.randn()
+            ke_new = 0.5 * m * v ** 2
+            Q += (ke_new - ke_old)
+
+            # half R step
+            x = x + ((dt / 2.0) * v)
+
+            # V step
+            v = v + ((dt) * force(x) / m)
+
+            # half R step
+            x = x + ((dt / 2.0) * v)
+
+            # half O step
+            ke_old = 0.5 * m * v ** 2
+            v = (a * v) + b * velocity_scale * np.random.randn()
+            ke_new = 0.5 * m * v ** 2
+            Q += (ke_new - ke_old)
+
+            xs[i] = x
+            vs[i] = v
+
+            # Update W_shads
+            E_new = potential(x) + 0.5 * m * v ** 2
+            W_shads[i] = (E_new - E_old) - Q
+
+        return xs, vs, Q, W_shads
+
+    return jit(simulate_orvro)
+
 # TODO: simulator that just stores trajectory in terms of histogram to start with
 # For now though, can just simulate in chunks.
 
