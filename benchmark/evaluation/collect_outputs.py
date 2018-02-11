a62da56f5a47773fa7e58e22eb568452a51b63f5
Josh Fass <jf694@cornell.edu>
2018-02-11 11:26:20 -0500

    Update result-collector script for parallel outputs

--- benchmark/evaluation/collect_outputs.py
+++ benchmark/evaluation/collect_outputs.py
@@ -5,33 +5,45 @@ which generates a single .pkl file per outer-loop sample.
 from pickle import load
 from glob import glob
 from simtk import unit
+from tqdm import tqdm
+import numpy as np
+from benchmark.evaluation.compare_near_eq_and_exact import estimate_from_work_samples
 
 def experiment_tuple_to_key(experiment):
+    """One of the experiment descriptors is unit'd -- strip unit."""
     (scheme, dt, marginal, testsystem) = experiment
     return (scheme, dt / unit.femtosecond, marginal, testsystem)
 
 def key_to_experiment_tuple(key):
+    """Re-add unit"""
     (scheme, dt, marginal, testsystem) = key
     return (scheme, dt * unit.femtosecond, marginal, testsystem)
 
 fnames = glob('*.pkl')
 
+print('reading all files')
 results = dict()
-for fname in fnames:
+for fname in tqdm(fnames):
     with open(fname, 'rb') as f:
+        l = load(f)
 
-        (experiment, result) = load(f)
+        (experiment, result) = l
+        Ws = list(result['Ws'])
 
         key = experiment_tuple_to_key(experiment)
         if key not in results:
-            results[key] = result
+            results[key] = Ws
         else:
-            results[key].append(result)
+            results[key].extend(Ws)
 
 print(results.keys())
 
 from pickle import dump
+print('saving all summaries')
+for i, key in enumerate(tqdm(results.keys())):
+    Ws = np.array(results[key])
+    new_estimate = np.mean([estimate_from_work_samples(w) for w in Ws])
+    result = {'Ws': Ws, 'new_estimate': new_estimate}
 
-for i, key in enumerate(results.keys()):
-    with open('summary_{}.pkl'.format(i), 'wb') as f:
+    with open('summary/summary_{}.pkl'.format(i), 'wb') as f:
         dump((key_to_experiment_tuple(key), results[key]), f)
